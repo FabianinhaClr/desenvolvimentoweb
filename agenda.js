1a3829e8365db665ac4cf8f1693ca36819453908
Clara Antonelo <110927475+FabianinhaClr@users.noreply.github.com>
2023-05-15 11:08:23 -0300

    Add files via upload

---
+++ agenda.js
@@ -0,0 +1,7 @@
+function login_submit(){
+    var nome = document.getElementById("nome_docente").value;
+    var siape = document.getElementById("siape_docente").value;
+    var email = document.getElementById("email_docente").value;
+    var area = document.getElementById("area_docente").value;
+    var carga = document.getElementById("carga_docente").value;
+}
